c29ee8aeeeecd868c61ae9a3a137fe6ee0d6fbe9
Gregory Jefferis <jefferis@gmail.com>
2016-02-29 18:05:46 +0000

    Update FAFB11 definition to include boundingbox/origin

--- data/templatebrains.R
+++ data/templatebrains.R
@@ -1,5 +1,8 @@
 FAFB11 <- nat.templatebrains::templatebrain(
   "FAFB11",
   type = 'Stitched serial section EM volume of whole adult female brain', sex = 'F',
-  dims = c(307041, 169832, 7062), voxdims = c(4,4,35), units = 'nm'
+  dims = c(307041, 169832, 7062), voxdims = c(4,4,35), units = 'nm',
+  BoundingBox=nat::boundingbox(nat::im3d(dims = c(307041, 169832, 7062),
+                                         voxdims = c(4,4,35))),
+  origin=c(0,0,0)
 )

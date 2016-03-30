e41a495e6735cf89229a028385611b5ab82407b2
Gregory Jefferis <jefferis@gmail.com>
2016-03-30 20:30:55 +0100

    make template

---
+++ R/make_template.R
@@ -0,0 +1,9 @@
+# Private function to make a template for a given CATMAID stack
+make_template <- function(stackinfo=catmaid::catmaid_fetch('1/stack/7/info'), ...){
+  i=nat::im3d(dims=unlist(stackinfo$dimension), voxdims=unlist(stackinfo$resolution))
+  v=stringr::str_match(stackinfo$stitle, "V(\\d+)")[2]
+  # TODO this is a bit of a hack due to
+  # https://github.com/jefferislab/nat.templatebrains/issues/25
+  attr(i,'file')=paste0("FAFB", v)
+  nat.templatebrains::as.templatebrain(i)
+}

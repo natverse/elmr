0588719f98dcce13da5f116c2d99802bac8aec02
Gregory Jefferis <jefferis@gmail.com>
2016-09-12 19:05:34 +0100

    tag template space of dense_core_neurons

--- data-raw/dense_core_neurons.R
+++ data-raw/dense_core_neurons.R
@@ -1,4 +1,5 @@
 # a pair of neurons that Greg traced that will be used as an example
 library(catmaid)
 dense_core_neurons <- read.neurons.catmaid(c(713968, 822008))
+regtemplate(dense_core_neurons) <- FAFB12
 devtools::use_data(dense_core_neurons, overwrite = T)

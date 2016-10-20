96777d31bb1f05d76d49e263a97784f2550cff43
Gregory Jefferis <jefferis@gmail.com>
2016-10-20 10:45:31 +0100

    Add prelim FAFB13 templatebrain object

--- data/templatebrains.R
+++ data/templatebrains.R
@@ -19,3 +19,15 @@ FAFB12 <- nat.templatebrains::templatebrain(
   BoundingBox = structure(c(0, 919160, 0, 571552, 0, 247170), .Dim = 2:3, class = "boundingbox"),
   units = "nm"
 )
+
+FAFB13 <- nat.templatebrains::templatebrain(
+  name = "FAFB13",
+  type = 'Stitched serial section EM volume of whole adult female brain',
+  sex = 'F',
+  # FIXME these dims are certainly wrong!
+  dims = c(229791L, 142889L, 7063L),
+  voxdims = c(4, 4, 35),
+  origin = c(0, 0, 0),
+  BoundingBox = structure(c(0, 919160, 0, 571552, 0, 247170), .Dim = 2:3, class = "boundingbox"),
+  units = "nm"
+)

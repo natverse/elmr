b03c2b307d399c38f28df0fbca835ddb20974324
Gregory Jefferis <jefferis@gmail.com>
2017-03-07 11:21:45 -0500

    fix namespace issues / NSE for dplyr functions in authorship
    * see https://cran.rstudio.com/web/packages/dplyr/vignettes/nse.html
    * use formulae to replace NSE

--- R/contributions.R
+++ R/contributions.R
@@ -15,7 +15,9 @@
 #'   \code{write_authors/write_ack} a string describing in descending order of
 #'   contribution written to the console by \code{\link{cat}}.
 #' @export
-#'
+#' @importFrom dplyr inner_join bind_rows summarise_ group_by_ arrange_ desc
+#'   mutate_ %>%
+#' @importFrom catmaid catmaid_get_contributor_stats catmaid_get_user_list
 #' @examples
 #' \donttest{
 #' pnsc=summarise_contribution("annotation:^PN$")
@@ -37,16 +39,16 @@ summarise_contribution <- function(skids,
 
   df <- if(type=="synapses"){
     bind_rows(stats[c("pre_contributors", "stats$post_contributors")]) %>%
-      group_by(id) %>%
-      summarise(n=sum(n)) %>%
+      group_by_(~id) %>%
+      summarise_(n=~sum(n)) %>%
       as.data.frame
   } else {
     stats[[paste0(sub("s$","",type),'_contributors')]]
   }
 
   stats.summ <- inner_join(df, uls, by='id') %>%
-    arrange(desc(n)) %>%
-    mutate(pct=n/sum(n)*100, cpct=cumsum(pct))
+    arrange_(~desc(n)) %>%
+    mutate_(pct=~n/sum(n)*100, cpct=~cumsum(pct))
   attr(stats.summ, 'type')=type
   stats.summ
 }
@@ -58,13 +60,14 @@ summarise_contribution <- function(skids,
 #'   length of 2, the first number is assumed to be the criterion for arbour
 #'   nodes, the second for synapses.
 #' @export
+#' @importFrom dplyr mutate_ filter_ %>%
 #' @rdname summarise_contribution
 suggest_authorship <- function(x, auth=5.0, ackn=c(3000, 100)) {
   if(length(ackn)>1)
     ackn=ifelse(attr(x,'type')=="nodes", ackn[1], ackn[2])
   x %>%
-    filter(n >= ackn) %>%
-    mutate(action = ifelse(pct >= auth, "auth", "ack"))
+    filter_(~n >= ackn) %>%
+    mutate_(action = ~ifelse(pct >= auth, "auth", "ack"))
 }
 
 #' @description \code{write_authors} produces a list of authors
@@ -72,16 +75,23 @@ suggest_authorship <- function(x, auth=5.0, ackn=c(3000, 100)) {
 #' @rdname summarise_contribution
 write_authors <- function(x, ...) {
   if(!'action' %in% names(x)) x=suggest_authorship(x, ...)
-  with(subset(x, action=='auth'), cat(paste(full_name, collapse=", ")))
+  xauth=x[x$action=='auth',]
+  cat(paste(xauth$full_name, collapse=", "))
 }
 
 #' @description \code{write_ack} produces an acknowledgements wording.
 #' @export
 #' @rdname summarise_contribution
 write_ack <- function(x, ...) {
-  if(!'action' %in% names(x)) x=suggest_authorship(x, ...)
-  with(subset(x, action=='ack'),
-       cat("We thank", paste(full_name, collapse=", "),
-           "for contributing", round(sum(pct), digits = 1),
-           "% of reconstructed arbour cable."))
+  if (!'action' %in% names(x))
+    x = suggest_authorship(x, ...)
+  xack = x[x$action == 'ack', ]
+
+  cat(
+    "We thank",
+    paste(xack$full_name, collapse = ", "),
+    "for contributing",
+    round(sum(xack$pct), digits = 1),
+    "% of reconstructed arbour cable."
+  )
 }

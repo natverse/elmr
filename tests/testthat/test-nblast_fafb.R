a3f8087a7507b943a1a9f6c3a177b852972b269d
Gregory Jefferis <jefferis@gmail.com>
2018-03-05 15:12:06 +0000

    WIP: test for printing NBLAST results

--- tests/testthat/test-nblast_fafb.R
+++ tests/testthat/test-nblast_fafb.R
@@ -1,6 +1,8 @@
-context("nblast_fafb")
+context("nblast_fafb2")
 
 test_that("print nblast_fafb result", {
+  assign("yu2010.dps", readRDS('testdata/yu2010.dps.rds'))
+  cat(ls(), file = 'out.txt')
   nblastres=readRDS('testdata/nblastres.rds')
   expect_known_output(nblastres, 'testdata/nblastres.print', print = T)
 })

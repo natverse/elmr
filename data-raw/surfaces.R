f82629bc2423191822d85c818c1fa9fecd86c0dd
Gregory Jefferis <jefferis@gmail.com>
2018-05-09 11:31:43 +0100

    Update FAFB.surf/FAFBNP.surf to match FAFB14 equivalents
    * they had got out of sync
    * still haven't found a way to avoid these duplicate objects

--- data-raw/surfaces.R
+++ data-raw/surfaces.R
@@ -9,3 +9,8 @@ FAFB14.surf=xform_brain(JFRC2.surf, sample = JFRC2, reference = FAFB14)
 FAFB14NP.surf=xform_brain(JFRC2NP.surf, sample = JFRC2, reference = FAFB14)
 use_data(FAFB14.surf, overwrite = TRUE)
 use_data(FAFB14NP.surf, overwrite = TRUE)
+
+FAFB.surf=FAFB14.surf
+FAFBNP.surf=FAFB14NP.surf
+use_data(FAFB.surf, overwrite = TRUE)
+use_data(FAFBNP.surf, overwrite = TRUE)

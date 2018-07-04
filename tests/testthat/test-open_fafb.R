7a696715cb6869ca8bacebf9832776aaa36bc179
Gregory Jefferis <jefferis@gmail.com>
2018-07-04 09:31:08 +0100

    Add tests for open_fafb

---
+++ tests/testthat/test-open_fafb.R
@@ -0,0 +1,30 @@
+context("test-open_fafb.R")
+
+test_that("open_fafb works", {
+  baseline <-
+    data.frame(
+      treenode_id = c(10362518L, 18699757L, 18699746L),
+      parent_id = c(10362517L, 18699755L, 18699745L),
+      x = c(363719, 455145, 455425),
+      y = c(149281, 244639, 245487),
+      z = c(181280, 34680, 33680),
+      confidence = c(5L, 5L, 5L),
+      radius = c(-1,-1,-1),
+      skid = c(16L, 16L, 16L),
+      edition_time = c(1490896452.62488, 1508753040.35593, 1508753020.87421),
+      user_id = c(117L, 167L, 167L),
+      url = c(
+        "https://neuropil.janelia.org/tracing/fafb/v14/?pid=1&zp=181280&yp=149281&xp=363719&tool=tracingtool&sid0=5&s0=1.000000&active_skeleton_id=16&active_node_id=10362518",
+        "https://neuropil.janelia.org/tracing/fafb/v14/?pid=1&zp=34680&yp=244639&xp=455145&tool=tracingtool&sid0=5&s0=1.000000&active_skeleton_id=16&active_node_id=18699757",
+        "https://neuropil.janelia.org/tracing/fafb/v14/?pid=1&zp=33680&yp=245487&xp=455425&tool=tracingtool&sid0=5&s0=1.000000&active_skeleton_id=16&active_node_id=18699746"
+      ),
+      stringsAsFactors = FALSE
+    )
+
+  testdf <- baseline[,-ncol(baseline)]
+  expect_equal(open_fafb(testdf), baseline$url)
+  expect_equal(open_fafb(testdf[1,]), baseline$url[1])
+  expect_equal(open_fafb(testdf[1,], active_node_id = testdf$treenode_id,
+                         active_skeleton_id = testdf$skid, open=FALSE),
+               baseline$url[1])
+})

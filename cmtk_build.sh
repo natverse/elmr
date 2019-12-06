72f5c2edcda6da3c7dfab5d59e99e78c93a224f1
Sri <j.sridharrajan@gmail.com>
2019-12-06 11:53:16 +0000

    Replaced the download of the build-cmtk script with actual source lines.

--- cmtk_build.sh
+++ cmtk_build.sh
@@ -1,6 +1,14 @@
 if ! [ -x "$(command -v cmtk)" ]; then
-   curl -OL https://raw.githubusercontent.com/jefferis/nat/master/build-cmtk.sh
-   bash build-cmtk.sh
+   #!/bin/sh
+	set -e
+	# check to see if cmtk install folder is empty
+	if [ ! -d "$HOME/usr/local/bin" ]; then
+  		mkdir -p $HOME/src && cd $HOME/src
+  		git clone --depth 50 https://github.com/jefferis/cmtk
+  		cd cmtk/core && mkdir build && cd build && cmake .. && make DESTDIR=$HOME/ all install
+	else
+  		echo 'Using cached $HOME/usr/local directory.';
+	fi
 else 
    echo 'cmtk already installed'
 fi

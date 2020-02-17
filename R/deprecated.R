e37bd62cf29acd5157955b9975809e162df4b6a0
Gregory Jefferis <jefferis@gmail.com>
2020-02-17 13:15:04 +0000

    (start) documenting defunct functions

---
+++ R/deprecated.R
@@ -0,0 +1,10 @@
+#' Functions that have been removed or moved to other packages
+#'
+#' @section moved:
+#' \itemize{
+#'
+#' \item \code{\link[nat]{distal_to}} has been moved to the \code{\link{nat}} package.
+#'
+#' }
+#' @name elmr-defunct
+NULL

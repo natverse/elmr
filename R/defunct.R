b845eab15f97e06edeb2e4683351d9de9f129ce3
Gregory Jefferis <jefferis@gmail.com>
2020-02-23 13:24:49 +0000

    Remove plot3d.ngraph

--- R/defunct.R
+++ R/defunct.R
@@ -1,15 +1,28 @@
 #' Functions that have been removed or moved to other packages
 #'
-#' @section moved:
-#' \itemize{
+#' @section moved: \itemize{
 #'
-#' \item \code{\link[nat]{distal_to}} has been moved to the \code{\link{nat}} package.
-#' \item \code{\link[nat]{simplify_neuron}} has been moved to the \code{\link{nat}} package.
-#' \item \code{\link[nat]{stitch_neuron}} has been moved to the \code{\link{nat}} package.
-#' \item \code{\link[nat]{stitch_neurons}} has been moved to the \code{\link{nat}} package.
-#' \item \code{\link[nat]{prune_twigs}} has been moved to the \code{\link{nat}} package.
-#' \item \code{\link[nat]{tpsreg}} has been moved to the \code{\link{nat}} package.
+#'   \item \code{\link[nat]{distal_to}} has been moved to the \code{\link{nat}}
+#'   package.
 #'
-#' }
+#'   \item \code{\link[nat]{simplify_neuron}} has been moved to the
+#'   \code{\link{nat}} package.
+#'
+#'   \item \code{\link[nat]{stitch_neuron}} has been moved to the
+#'   \code{\link{nat}} package.
+#'
+#'   \item \code{\link[nat]{stitch_neurons}} has been moved to the
+#'   \code{\link{nat}} package.
+#'
+#'   \item \code{\link[nat]{prune_twigs}} has been moved to the
+#'   \code{\link{nat}} package.
+#'
+#'   \item \code{\link[nat]{tpsreg}} has been moved to the \code{\link{nat}}
+#'   package.
+#'
+#'   \item \code{\link[nat]{plot3d.ngraph}} has been moved to the
+#'   \code{\link{nat}} package.
+#'
+#'   }
 #' @name elmr-defunct
 NULL
